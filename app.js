d5b16420ee1dd8ffacb8418b09b4106f7a13846a
lth <lth@netcompartner.com>
2017-10-07 00:33:46 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,121 @@
+/*
+ * App
+ */
+(function() {
+
+    var appName = "split-flap";
+
+    var app = angular.module(appName, [
+    ]);
+
+    app.controller("AppController", ["$log", "$scope", "$interval", 
+        function($log, $scope, $interval) {
+            $log.debug("AppController: starting");
+            
+            var that = this;
+            that.length = 20;
+            that.value = "";
+
+            that.interval = $interval(function() {
+                that.value = "Hello\xa0World\xa0" + new Date().toTimeString();
+            }, 1000);
+            
+            $scope.$on("$destroy", function() {
+                $interval.cancel(that.interval);
+            });
+        }
+    ]);
+
+    app.directive("splitFlap", ["$log", 
+        function($log) {
+            return {
+                restrict: "E", 
+                replace: true, 
+                scope: {
+                    value: "@", 
+                    length: "@"
+                }, 
+                controller: ["$log", "$scope", 
+                    function($log, $scope) {
+                        $log.debug("SplitFlapController: starting - length = " + $scope.length);
+
+                        var that = this;
+                        that.characters = [];
+                        
+                        for (var i = 0; i < $scope.length; ++i) {
+                            that.characters.push("\xa0");
+                        }
+
+                        that.setCharacters = function(value) {
+                            for (var i = 0; i < value.length; ++i) {
+                                that.characters[i] = value.charAt(i);
+                            }
+                        }
+
+                        $scope.$watch("value", function() {
+                            //$log.debug("SplitFlapController: value has changed - now = %o", $scope.value);
+                            that.setCharacters($scope.value.substring(0, $scope.length));
+                        });
+
+                    }
+                ], 
+                controllerAs: "splitFlapCtrl", 
+                template: '<div class="split-flap"><split-flap-character ng-repeat="character in splitFlapCtrl.characters track by $index" value="{{character}}"/></div>'
+            }
+        }
+    ]);
+
+    app.directive("splitFlapCharacter", [
+        function() {
+            return {
+                restrict: "E", 
+                replace: true, 
+                scope: {
+                    value: "@"
+                }, 
+                controller: ["$log", "$scope", "$timeout", 
+                    function($log, $scope, $timeout) {
+                        $log.debug("SplitFlapCharacerController: starting");
+
+                        var deck = "\xa0ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.-!:;";
+                        var deckIdx = 0;
+                        var timeout = null;
+
+                        var that = this;
+                        that.character = deck.charAt(deckIdx);
+
+                        that.moveTo = function(character) {
+                            if (timeout) {
+                                $timeout.cancel(timeout);
+                            }
+                            timeout = $timeout(function() {
+                                deckIdx = deckIdx + 1;
+                                if (deckIdx >= deck.length) {
+                                    deckIdx = 0;
+                                }
+                                that.character = deck.charAt(deckIdx);
+                                
+                                if (character != that.character) {
+                                    that.moveTo(character);
+                                }
+                            }, 50);
+
+                        };
+
+                        $scope.$watch("value", function() {
+                            //$log.debug("SplitFlapCharacterController: value changed - now = %o", $scope.value);
+                            //that.character = $scope.value;
+                            that.moveTo($scope.value);
+                        });
+                    }
+                ], 
+                controllerAs: "splitFlapCharacterCtrl", 
+                template: '<span class="split-flap-character">{{splitFlapCharacterCtrl.character}}</span>'
+            };
+        }
+    ]);
+
+})();
+/*
+ * vim: ts=4 et nowrap autoindent
+ */
